f060d34783bee198d0ba122279900d07394ece7d
CeliaFernandez <celia.fernandez.madrazo@cern.ch>
2023-02-22 13:09:44 +0100

    Lo del edm::EDProducer que se ha quitado y genera conflicto con 130X

--- plugins/segmentAnalyzer.cc
+++ plugins/segmentAnalyzer.cc
@@ -2,7 +2,6 @@
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/one/EDAnalyzer.h"
-#include "FWCore/Framework/interface/EDProducer.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
